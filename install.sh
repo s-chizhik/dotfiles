c3f9bfe820ac700a5237073be8e3b6b309343499
Sergey Chizhik <s-chizhik@users.noreply.github.com>
2018-02-08 23:26:48 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install zsh git wget curl htop tmux whois tree pv mc vim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
