e2a2fd225d9b4d9a9b9415425848943807e619db
Sergey Chizhik <s-chizhik@users.noreply.github.com>
2018-07-17 09:34:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@ sudo apt-get update
 sudo apt-get install zsh git wget curl htop tmux whois tree pv mc vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/colors/darcula.vim --create-dirs https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/s-chizhik/dotfiles/master/.vimrc
+
+curl -fLo ~/.gitconfig https://raw.githubusercontent.com/s-chizhik/dotfiles/master/.gitconfig
